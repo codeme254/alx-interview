9a9b20dd523bb43aa39c28b242169caea50d3d5d
codeme254 <otwomadennis365@gmail.com>
2023-04-19 05:58:07 -0400

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Create a pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal triangle of n
+    """
+    triangle = []
+    for i in range(1, n+1):
+        triangle.append(pascal_row(i))
+    return triangle
+
+
+def pascal_row(row_num):
+    """
+    Creates a pascal row value given an integer
+    eg 1 -> 1
+    2 -> 1 1
+    3 -> 1 2 1
+    """
+    row = []
+    for i in range(row_num):
+        if i == 0 or i == row_num-1:
+            row.append(1)
+        else:
+            prev_row = pascal_row(row_num-1)
+            row.append(prev_row[i-1] + prev_row[i])
+    return row
