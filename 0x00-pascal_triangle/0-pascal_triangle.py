765ccb2089b1fddf4ebcab2273af76ef7a530949
codeme254 <otwomadennis365@gmail.com>
2023-04-19 06:11:51 -0400

    hot fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,25 +9,9 @@ def pascal_triangle(n):
     returns a list of lists of integers representing the Pascal triangle of n
     """
     triangle = []
-    for i in range(1, n+1):
-        triangle.append(pascal_row(i))
+    for i in range(n):
+        row = [1] * (i+1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
     return triangle
-
-
-def pascal_row(row_num):
-    """
-    Creates a pascal row value given an integer
-    eg 1 -> 1
-    2 -> 1 1
-    3 -> 1 2 1
-    """
-    row = []
-    if row_num <= 0:
-        return []
-    for i in range(row_num):
-        if i == 0 or i == row_num-1:
-            row.append(1)
-        else:
-            prev_row = pascal_row(row_num-1)
-            row.append(prev_row[i-1] + prev_row[i])
-    return row
