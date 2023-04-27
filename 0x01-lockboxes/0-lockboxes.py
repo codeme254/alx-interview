b46b4d7ebea6dc3a94911049067699a1a8216153
codeme254 <otwomadennis365@gmail.com>
2023-04-27 14:05:20 +0300

    fix lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,6 +7,58 @@ Each box may contain keys to the other boxes.
 """
 
 
+# def canUnlockAll(boxes):
+#     """
+#     Determines if all the boxes can be unlocked
+#     boxes is a list of lists
+#     A key with the same number as a box opens that box
+#     You can assume all keys will be positive integers
+#     There can be keys that do not have boxes
+#     The first box boxes[0] is unlocked
+#     Return True if all boxes can be opened, else return False
+#     """
+
+#     boxesUnlockInformation = {}
+
+#     boxesUnlockInformation[0] = 'opened'
+
+#     for k in range(1, len(boxes)):
+#         boxesUnlockInformation[k] = 'closed'
+
+#     for i in range(0, len(boxes)):
+#         if boxesUnlockInformation[i] == 'opened':
+#             current_box = boxes[i]
+#             for i in range(0, len(current_box)):
+#                 boxesUnlockInformation[i] = 'opened'
+#                 newOpenBox = boxes[i]
+#                 for i in range(0, len(newOpenBox)):
+#                     boxesUnlockInformation[newOpenBox[i]] = 'opened'
+#         else:
+#             # not opened
+#             # look for the key in all opened boxes and if you find it,
+#             open the
+#             # current box
+#             for key, value in boxesUnlockInformation.items():
+#                 if value == 'opened':
+#                     # look for i and if you find it set
+#                     # boxesUnlockInformation[i] to opened
+#                     if key <= len(boxes) - 1:
+#                         openBox = boxes[key]
+#                         for j in range(0, len(openBox)):
+#                             if openBox[j] == i:
+#                                 boxesUnlockInformation[i] = 'opened'
+
+#     # print(boxesUnlockInformation)
+#     for key, value in boxesUnlockInformation.items():
+#         if value == 'closed':
+#             return False
+
+#     return True
+
+# # canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]])
+# # canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
+
+
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be unlocked
@@ -17,42 +69,14 @@ def canUnlockAll(boxes):
     The first box boxes[0] is unlocked
     Return True if all boxes can be opened, else return False
     """
-
-    boxesUnlockInformation = {}
-
-    boxesUnlockInformation[0] = 'opened'
-
-    for k in range(1, len(boxes)):
-        boxesUnlockInformation[k] = 'closed'
-
-    for i in range(0, len(boxes)):
-        if boxesUnlockInformation[i] == 'opened':
-            current_box = boxes[i]
-            for i in range(0, len(current_box)):
-                boxesUnlockInformation[i] = 'opened'
-                newOpenBox = boxes[i]
-                for i in range(0, len(newOpenBox)):
-                    boxesUnlockInformation[newOpenBox[i]] = 'opened'
-        else:
-            # not opened
-            # look for the key in all opened boxes and if you find it, open the
-            # current box
-            for key, value in boxesUnlockInformation.items():
-                if value == 'opened':
-                    # look for i and if you find it set
-                    # boxesUnlockInformation[i] to opened
-                    if key <= len(boxes) - 1:
-                        openBox = boxes[key]
-                        for j in range(0, len(openBox)):
-                            if openBox[j] == i:
-                                boxesUnlockInformation[i] = 'opened'
-
-    # print(boxesUnlockInformation)
-    for key, value in boxesUnlockInformation.items():
-        if value == 'closed':
-            return False
-
-    return True
-
-# canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]])
-# canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
